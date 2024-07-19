d030be6dba74aaf166863a9791299af3fc7361ee
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2024-07-20 00:59:32 +0530

    Update page.jsx

--- src/app/(pagelogin)/login/page.jsx
+++ src/app/(pagelogin)/login/page.jsx
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 
-export default async function(){
+export default async function LoginPage(){
 
     const session = await getServerSession(authOptions);
     console.log(session);
